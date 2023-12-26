f248acbfca7ceb1ad8861e9b487e7fadfa840b66
ShinjiMC <nayapbrau@gmail.com>
2023-12-26 00:48:09 -0500

    Adding GTEST and Check Delete Node in KDTree

--- KDTree/includes/Node.hpp
+++ KDTree/includes/Node.hpp
@@ -5,25 +5,31 @@ using namespace std;
 
 using Point3D = vector<double>;
 
-class Node {
-    public:
-        Point3D point;
-        Node * left;
-        Node * right;
+class Node
+{
+public:
+    Point3D point;
+    Node *left;
+    Node *right;
 
-        Node(){}
-        Node(const Point3D& point){
-            left = nullptr;
-            right = nullptr;
-            this->point = point;
-        }
-        ~Node(){}
+    Node() {}
+    Node(const Point3D &point)
+    {
+        left = nullptr;
+        right = nullptr;
+        this->point = point;
+    }
+    ~Node()
+    {
+        Point3D().swap(point);
+    }
 };
 
-ostream& operator<<(ostream& os, const Point3D& point) {
+ostream &operator<<(ostream &os, const Point3D &point)
+{
     os << "(";
-    for (int i=0; i<point.size(); i++)
-        os << point[i] << ( i!=point.size()-1 ? "," : "");
+    for (int i = 0; i < point.size(); i++)
+        os << point[i] << (i != point.size() - 1 ? "," : "");
     os << ")";
     return os;
 }
