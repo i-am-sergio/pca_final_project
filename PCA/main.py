8c03048921a170d0ff1afd157276239094ea4712
PaulParizacaMozo <pparizaca@unsa.edu.pe>
2023-12-27 03:34:24 -0500

    Add PCA Graphics

--- PCA/main.py
+++ PCA/main.py
@@ -52,21 +52,57 @@ plt.title('Círculo de correlacion')
 plt.xlabel('PCA1')
 plt.ylabel('PCA2')
 # Agregar un vector desde el origen a cada punto
+#for i in range(len(pca_impl.get_df_pca2_transpose())):
+#    plt.quiver(0, 0, pca_impl.get_df_pca3_transpose()['PCA1'][i], pca_impl.get_df_pca3_transpose()['PCA2'][i], angles='xy', scale_units='xy', scale=1, color='red', width=0.005)
+#plt.show()
 for i in range(len(pca_impl.get_df_pca2_transpose())):
     plt.quiver(0, 0, pca_impl.get_df_pca3_transpose()['PCA1'][i], pca_impl.get_df_pca3_transpose()['PCA2'][i], angles='xy', scale_units='xy', scale=1, color='red', width=0.005)
+    plt.text(pca_impl.get_df_pca3_transpose()['PCA1'][i], pca_impl.get_df_pca3_transpose()['PCA2'][i], f'Var. {i+1}', color='blue', fontsize=8)
+
+plt.xlabel('PCA1')
+plt.ylabel('PCA2')
+plt.title('Circulo de correlacion con 2 componentes')
+plt.show()
+
+# Graficar Grafico de tabla 2D
+
+sns.scatterplot(data=pca_impl.get_df_pca2(), x="PCA1", y="PCA2", color="green", s=10)
+circle = plt.Circle((0, 0), 70, color='blue', fill=False, linestyle="-")
+plt.gca().add_patch(circle)
+# Configurar la proporción de los ejes para que sean iguales
+plt.axis('equal')
+# Configuración adicional
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+plt.title('Círculo de correlacion')
+plt.xlabel('PCA1')
+plt.ylabel('PCA2')
+plt.title('Mapa de Factores Individuales')
 plt.show()
 
 
-
 # Graficar Grafico de correlacion 3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# Obtener los datos
 x = pca_impl.get_df_pca3_transpose()["PCA1"]
 y = pca_impl.get_df_pca3_transpose()["PCA2"]
 z = pca_impl.get_df_pca3_transpose()["PCA3"]
-ax.scatter(x, y, z) #, c='green', marker='o')
-# Agregar quivers desde el origen a cada punto
+
+# Crear un DataFrame
+df_points = pd.DataFrame({'X': x, 'Y': y, 'Z': z})
+
+# Directorio donde se guardará el archivo CSV
+directory = '../KDTree'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+csv_path = os.path.join(directory, 'puntos.csv')
+df_points.to_csv(csv_path, header=False, index=False)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(x, y, z, color='blue', marker='o')
+
 ax.quiver(0, 0, 0, x, y, z, color='red', arrow_length_ratio=0.05, linewidth=1, linestyle='dashed')
+
 # Crear una elipse en 3D con radio 80
 u = np.linspace(0, 2 * np.pi, 100)
 v = np.linspace(0, np.pi, 100)
@@ -74,9 +110,12 @@ x_elipse = 80 * np.outer(np.cos(u), np.sin(v))
 y_elipse = 80 * np.outer(np.sin(u), np.sin(v))
 z_elipse = 80 * np.outer(np.ones(np.size(u)), np.cos(v))
 ax.plot_wireframe(x_elipse, y_elipse, z_elipse, color='skyblue', linewidth=1, alpha=0.1)
+
+# Configuraciones adicionales
 ax.set_xlabel('PCA1')
 ax.set_ylabel('PCA2')
 ax.set_zlabel('PCA3')
+plt.title('Círculo de correlación con 3 componentes')
 plt.show()
 
 
